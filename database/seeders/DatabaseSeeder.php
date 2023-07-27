ca80f30801a18e51dc1ac939e38cc543b91a3bb6
calsob <admin@calsob.com>
2023-07-27 15:30:21 +0530

    admin seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+        // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create(['name' => 'Abhishek Kumar', 'email' => 'admin@calsob.com', 'password' => 'password']);
+        Role::create(['name' => 'Admin', 'guard_name' => 'web', '_lft' => 1, '_rgt' => 2, 'parent_id' => NUll]);
+        Permission::create(['name' => 'authentication', 'guard_name' => 'web']);
+        DB::table('model_has_roles')->insert(['role_id' => 1,'model_type' => 'App\\Models\\User','model_id' => 1]);
+        DB::table('role_has_permissions')->insert(['permission_id' => 1,'role_id' => 1]);
+
+
+
+
+
     }
 }
