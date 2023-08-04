6779684a75834f01fd45d28c4efe07f7341e54d4
calsob <admin@calsob.com>
2023-08-04 16:43:40 +0530

    updated condition for task for review

--- app/Filament/Resources/TaskreviewResource/Pages/ListTaskreviews.php
+++ app/Filament/Resources/TaskreviewResource/Pages/ListTaskreviews.php
@@ -12,6 +12,6 @@ class ListTaskreviews extends ListTasks
         $user_role_id = \Auth::user()->roles()->first()->id;
         $subordinates_role_id = \Auth::user()->roles()->first()->descendants->pluck('id')->toArray();
         array_push($subordinates_role_id,$user_role_id);
-        return static::getResource()::getEloquentQuery()->whereIn('role_id',$subordinates_role_id)->where('review',true);
+        return static::getResource()::getEloquentQuery()->whereIn('role_id',$subordinates_role_id)->where('review',true)->where('done',false);
     }
 }
